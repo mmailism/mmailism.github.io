1a37202743fca85449c2b3b04941c930afa75062
mailism <domiam09@gmail.com>
2024-07-17 16:24:12 +0700

    edit-js

--- js/main.js
+++
@@ -1,1 +0,0 @@
-<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
---
+++ src/script.js
@@ -0,0 +1,28 @@
+/* ========================= typing animation ========================= */
+var typed = new Typed(".typing",{
+    strings:["","Web Designer","Web Developer","Graphic Designer"],
+    typeSpeed:100,
+    backSpeed:60,
+    loop:true
+})
+/* ========================= aside ========================= */
+const nav = document.querySelector(".nav"),
+    navList = nav.querySelectorAll("li"),
+    totalNavList = navList.length;
+    for(let i=0; i<totalNavList; i++)
+    {
+        const a = navList[i].querySelector("a");
+    a.addEventListener("click", function()
+    {
+        for(let j=0; j<totalNavList; j++)
+        {
+            navList[j].querySelector("a").classList.remove("active");
+        }
+        this.classList.add("active")
+        showSection(this);
+    })
+    }
+    function showSection(element)
+    {
+        console.log(element.getAttribute("href").split("#");
+    }
